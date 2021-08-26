6b0a4e1bb0b749e550e3dedf8d170013629c9b27
Gabriel Santos Lima <gabriel.lyma16@gmail.com>
2021-08-26 05:59:00 -0300

    --- Auto-Git Commit ---

--- 001/index.js
+++ 001/index.js
@@ -1,43 +1,44 @@
-class Stars extends HTMLElement, Gpx {
+class ComponentUsedGpx extends HTMLElement {
     constructor(){
         super();
-        const shadow = this.attachShadow({ mode: 'open' })
-        
-        this.styled({
-            backgroundColor: 'red',
-            width: '150px',
-
-        })
-
-        shadow.appendChild(this.styles(`
-            .hello .world > div {
-                background-color: # fff;
-            }
-        `))
+        this.build();
     }
 
     build(){
+        const shadow = this.attachShadow({ mode: 'open' })
+    
 
+        this.styled({
+            backgroundColor: 'red',
+            width: '150px',
+        })
+
+        shadow.appendChild(this.styles(`
+            div {
+                background-color: # fff;
+            }
+        `))
+
+
+        const root = document.createElement('div')
+
+        shadow.appendChild(root)
     }
-
+    
     styles(atributes){
         const style = document.createElement('style')
         style.textContent = atributes
         
         return style
     }
-}
-
-class Gpx {
-    constructor(){
-
-    }
 
     styled(atributes = new CSSStyleDeclaration()){
         Object.entries(atributes).forEach(([attr, value]) => this.style[attr] = value)
         console.log(this.style)
     }
+    
 
+   
 }
 
-customElements.define('stars-rater', Stars)
+customElements.define('stars-rater', ComponentUsedGpx)
