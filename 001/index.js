3f114b09063a55fc2347b6e2920b4adc2f8c2be7
Gabriel Santos Lima <gabriel.lyma16@gmail.com>
2021-08-26 06:12:36 -0300

    St Custom

--- 001/index.js
+++ 001/index.js
@@ -1,44 +1,51 @@
 class ComponentUsedGpx extends HTMLElement {
-    constructor(){
+    constructor() {
         super();
         this.build();
     }
 
-    build(){
+    build() {
         const shadow = this.attachShadow({ mode: 'open' })
-    
+
+
+        const root = document.createElement('div')
+        root.classList.add('container')
+        root.innerHTML = `Hello, from customElement`
 
         this.styled({
-            backgroundColor: 'red',
-            width: '150px',
+            padding: '10px',
+            backgroundColor: 'blue',
         })
 
         shadow.appendChild(this.styles(`
-            div {
-                background-color: # fff;
+            .container {
+                background-color: purple;
+                color: white;
+                padding: 10px;
+                width: '150px',
+                height: '150px',
             }
         `))
 
 
-        const root = document.createElement('div')
 
         shadow.appendChild(root)
     }
-    
-    styles(atributes){
+
+    styles(atributes) {
         const style = document.createElement('style')
         style.textContent = atributes
-        
+
         return style
     }
 
-    styled(atributes = new CSSStyleDeclaration()){
+    styled(atributes = new CSSStyleDeclaration()) {
         Object.entries(atributes).forEach(([attr, value]) => this.style[attr] = value)
         console.log(this.style)
     }
-    
 
-   
+
+
 }
 
 customElements.define('stars-rater', ComponentUsedGpx)
