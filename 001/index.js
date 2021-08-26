ae786bfb972d31e336326aaa0416448d3659c8c1
Gabriel Santos Lima <gabriel.lyma16@gmail.com>
2021-08-26 05:28:41 -0300

    --- Auto-Git Commit ---

--- 001/index.js
+++ 001/index.js
@@ -1,9 +1,41 @@
-class Stars extends HTMLElement {
+class Stars extends HTMLElement, Gpx {
     constructor(){
         super();
         const shadow = this.attachShadow({ mode: 'open' })
+        
+        this.styled({
+            backgroundColor: 'red',
+            width: '150px',
 
-        shadow.innerHTML = 'Hello, world!'
+        })
+
+        shadow.appendChild(this.styles(`
+            .hello .world > div {
+                background-color: # fff;
+            }
+        `))
+    }
+
+    build(){
+
+    }
+
+    styles(atributes){
+        const style = document.createElement('style')
+        style.textContent = atributes
+        
+        return style
+    }
+}
+
+class Gpx {
+    constructor(){
+
+    }
+
+    styled(atributes = new CSSStyleDeclaration()){
+        Object.entries(atributes).forEach(([attr, value]) => this.style[attr] = value)
+        console.log(this.style)
     }
 
 }
